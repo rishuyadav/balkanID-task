902eaaecc9a97d9da8f4b49d3cbbaeaebd59bb93
Rishu Yadav <rishuss1212@gmail.com>
2023-04-08 18:23:06 +0530

    Feat: also fetching org data and converted raw sql to sqlAlchemy

--- dbconnection.py
+++ dbconnection.py
@@ -7,6 +7,17 @@ import psycopg2
 import csv
 from tenacity import retry, stop_after_attempt, wait_fixed
 from urllib.parse import urlencode
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+# Create database connection
+engine = create_engine('postgresql://postgres:postgres@127.0.0.1:5432/demo')
+Session = sessionmaker(bind=engine)
+
+# Create declarative base
+Base = declarative_base()
+
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 app.secret_key = "my_secret_key"
@@ -27,9 +38,8 @@ def home():
     logging.info("Home page accessed.")
     return render_template('home.html', oauth_url=url_for('oauth_login'))
 
+
 # OAuth login
-
-
 @app.route('/oauth/login')
 def oauth_login():
     logging.info("OAuth login initiated.\n")
@@ -41,9 +51,8 @@ def oauth_login():
     }
     return redirect(AUTHORIZE_URL + "?" + urlencode(params))
 
+
 # OAuth callback
-
-
 @app.route('/oauth/callback')
 def oauth_callback():
     logging.info("OAuth callback initiated.")
@@ -91,7 +100,9 @@ def fetch_data():
 
     # Create CSV file
     try:
-        create_csv_file(normalized_data)
+        session_ = Session()
+        query = session_.query(Owner.id, Owner.name, Owner.email, Repo.id, Repo.name, Repo.status, Repo.stars_count).join(Repo).all()
+        create_csv_file(query)
         return render_template('result.html', oauth_login=url_for('oauth_login'), download_csv=url_for('download_csv'))
     except Exception as e:
         logging.error("An error occurred while writing data to CSV: " + str(e))
@@ -105,10 +116,16 @@ def fetch_repos(access_token):
         "Authorization": f"Bearer {access_token}",
         "Accept": "application/vnd.github.v3+json"
     }
-    url = "https://api.github.com/user/repos"
-    response = requests.get(url, headers=headers)
-    response.raise_for_status()
-    return response.json()
+    user_repos_url = "https://api.github.com/user/repos"
+    response = requests.get(user_repos_url, headers=headers)
+    user_repos = response.json()
+
+    org_repos_url = "https://api.github.com/orgs/{org}/repos"
+    org_name = "ACM-VIT"
+    response = requests.get(org_repos_url.format(org=org_name), headers=headers)
+    org_repos = response.json()
+    # Return both user and organization repositories
+    return user_repos + org_repos
 
 
 # Function to normalize data and deduplicate
@@ -135,75 +152,72 @@ def normalize_data(repos):
     return normalized_data
 
 
-# Function to store data in Postgres database
+# Define owners and repos tables
+class Owner(Base):
+    __tablename__ = 'owners'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    email = Column(String)
+
+class Repo(Base):
+    __tablename__ = 'repos'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    status = Column(String)
+    stars_count = Column(Integer)
+    owner_id = Column(Integer, ForeignKey('owners.id'))
+
+# Define function to store data
 def store_data(normalized_data):
     try:
-        conn = psycopg2.connect(
-            database="demo", user="postgres", password="postgres", host="127.0.0.1", port="5432")
-        logging.info("Connected to database successfully.")
+        # Create session
+        session = Session()
 
         # Create tables if they don't exist
-        cursor = conn.cursor()
-        cursor.execute("""
-        CREATE TABLE IF NOT EXISTS owners (
-            id INTEGER PRIMARY KEY,
-            name TEXT,
-            email TEXT
-        );
-    """)
-
-        cursor.execute("""
-        CREATE TABLE IF NOT EXISTS repos (
-            id INTEGER,
-            name TEXT,
-            status TEXT,
-            stars_count INTEGER,
-            owner_id INTEGER REFERENCES owners(id),
-            PRIMARY KEY (id, owner_id)
-        );
-    """)
-
-        logging.info("Tables created successfully.")
+        Base.metadata.create_all(engine)
 
         # Insert data into the tables
         for data in normalized_data:
             owner = data['owner']
             repo = data['repo']
-            cursor.execute("""
-        INSERT INTO owners (id, name, email) VALUES (%s, %s, %s)
-        ON CONFLICT (id) DO UPDATE SET name = excluded.name, email = excluded.email;
-    """, (owner['id'], owner['name'], owner['email']))
-            cursor.execute("""
-        INSERT INTO repos (id, name, status, stars_count, owner_id) VALUES (%s, %s, %s, %s, %s)
-        ON CONFLICT (id, owner_id) DO UPDATE SET name = excluded.name, status = excluded.status, stars_count = excluded.stars_count;
-    """, (repo['id'], repo['name'], repo['status'], repo['stars_count'], repo['owner_id']))
-            conn.commit()
-    except(Exception, psycopg2.Error) as error:
-        logging.error("Error while connecting to database.", error)
+
+            # Upsert owners table
+            owner_obj = Owner(id=owner['id'], name=owner['name'], email=owner['email'])
+            session.merge(owner_obj)
+
+            # Upsert repos table
+            repo_obj = Repo(id=repo['id'], name=repo['name'], status=repo['status'], stars_count=repo['stars_count'], owner_id=owner['id'])
+            session.merge(repo_obj)
+
+        # Commit the changes to the database
+        session.commit()
+        logging.info("Data stored successfully.")
+
+    except Exception as error:
+        logging.error("Error while storing data.", error)
 
     finally:
-        if(conn):
-            cursor.close()
-            conn.close()
-            logging.info("Connection closed.")
+        if session:
+            session.close()
+            logging.info("Session closed.")
+
 
 
 # Function to create CSV file
-def create_csv_file(normalized_data):
+def create_csv_file(query_fetched_data):
+
     try:
         with open('repos.csv', mode='w', newline='') as file:
             writer = csv.writer(file)
             writer.writerow(['Owner ID', 'Owner Name', 'Owner Email',
                             'Repo ID', 'Repo Name', 'Status', 'Stars Count'])
-            for data in normalized_data:
-                owner = data['owner']
-                repo = data['repo']
-                writer.writerow([owner['id'], owner['name'], owner['email'],
-                                repo['id'], repo['name'], repo['status'], repo['stars_count']])
-        return "Data fetched and stored successfully."
+            for data in query_fetched_data:
+                writer.writerow(list(data))
+        print("Data fetched and stored successfully.")
     except Exception as e:
-        logging.error("An error occurred while writing data to CSV: " + str(e))
-        return "An error occurred while writing data to CSV: " + str(e)
+        print(f"An error occurred while writing data to CSV: {e}")
 
 
 # Download CSV file
